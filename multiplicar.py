c912ef829a4360289c0cf96813a51f45d50b3149
Angela <angela@MacBook-Pro-de-Angela-3.local>
2019-02-07 13:05:30 +0100

    Tablas de multiplicar

---
+++ multiplicar.py
@@ -0,0 +1,11 @@
+
+for numero1 in range(1,11):
+
+   print("Tabla del " + str(numero1))
+   print("-----------")
+
+   for numero2 in range(1,11):
+
+       mult = numero1*numero2
+
+       print(str(numero1) + " por " + str(numero2) + " es " + str(mult))
